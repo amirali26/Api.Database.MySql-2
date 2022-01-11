8765a9f72d0a540e825b1b2262ae5a794f2e6127
amirali26 <amiralishojaee123@gmail.com>
2022-01-11 00:04:23 +0000

    initial Commit

---
+++ Migrations/20211231141024_updated-account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class updatedaccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisteredDate",
+                table: "Accounts",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Size",
+                table: "Accounts",
+                type: "nvarchar(24)",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Website",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredDate",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Website",
+                table: "Accounts");
+        }
+    }
+}
---
+++ Migrations/20220101205856_account-user-invitation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class accountuserinvitation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountUserInvitations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ExternalId = table.Column<string>(type: "text", nullable: true),
+                    UserEmail = table.Column<string>(type: "text", nullable: true),
+                    ReferUserId = table.Column<int>(type: "int", nullable: true),
+                    AccountId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountUserInvitations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountUserInvitations_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AccountUserInvitations_Users_ReferUserId",
+                        column: x => x.ReferUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountUserInvitations_AccountId",
+                table: "AccountUserInvitations",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountUserInvitations_ReferUserId",
+                table: "AccountUserInvitations",
+                column: "ReferUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountUserInvitations");
+        }
+    }
+}
