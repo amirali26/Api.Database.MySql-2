c81530589c71d50046ac65cd1b211d8503bbd2b1
amirali26 <amiralishojaee123@gmail.com>
2022-02-06 15:51:56 +0000

    added migrations

---
+++ Migrations/20220206154750_updated-account-permissions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class updatedaccountpermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AreaInRegion",
+                table: "Requests",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostCode",
+                table: "Requests",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Requests",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaInRegion",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "PostCode",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Requests");
+        }
+    }
+}
