ada147fd713f67ff5ac51edb6f38a42a66ac83a5
amirali26 <amiralishojaee123@gmail.com>
2022-02-21 21:37:12 +0000

    add-account-validation

---
+++ Migrations/20220221213659_add-account-validation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class addaccountvalidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirmVerificationCode",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirmVerified",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirmVerificationCode",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "FirmVerified",
+                table: "Accounts");
+        }
+    }
+}
