bf621cc93adb5dcc53da8927135b038c097479a0
amirali26 <amiralishojaee123@gmail.com>
2022-02-21 19:23:50 +0000

    account-location added

---
+++ Migrations/20220221192331_account-location.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class accountlocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AreaInRegion",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostCode",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaInRegion",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "PostCode",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Accounts");
+        }
+    }
+}
