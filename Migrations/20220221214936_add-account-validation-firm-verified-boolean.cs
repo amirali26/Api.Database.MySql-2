9fd484c5b7f82677d193676e9f978766a5c01e55
amirali26 <amiralishojaee123@gmail.com>
2022-02-21 21:51:25 +0000

    wip

---
+++ Migrations/20220221214936_add-account-validation-firm-verified-boolean.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class addaccountvalidationfirmverifiedboolean : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "FirmVerified",
+                table: "Accounts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirmVerified",
+                table: "Accounts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+        }
+    }
+}
