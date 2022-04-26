1e70038e4230ad642d52874bf1b9ac06a08281a3
amirali26 <amiralishojaee123@gmail.com>
2022-04-26 19:30:05 +0100

    updated migrations

---
+++ Migrations/20220426182839_updated-user-approval.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class updateduserapproval : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "UserApproval",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "UserApproval");
+        }
+    }
+}
