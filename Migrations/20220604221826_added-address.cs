c13490e6c95241e98eca45801e6a5405cb8ef5a9
amirali26 <amiralishojaee123@gmail.com>
2022-06-04 23:21:25 +0100

    Added address as new column

---
+++ Migrations/20220604221826_added-address.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class addedaddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Accounts");
+        }
+    }
+}
