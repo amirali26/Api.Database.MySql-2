ef20440fb4d504c60a0d48b5e1951eb768161d76
amirali26 <amiralishojaee123@gmail.com>
2022-06-05 00:12:04 +0100

    added show-phone-number to requests

---
+++ Migrations/20220604231126_add-show-phone-number.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class addshowphonenumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowPhoneNumber",
+                table: "Requests",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowPhoneNumber",
+                table: "Requests");
+        }
+    }
+}
