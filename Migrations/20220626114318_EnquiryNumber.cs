ab4fbbbd32d6a3e3b5452383287494c9c9fc5db7
amirali26 <amiralishojaee123@gmail.com>
2022-06-26 12:43:41 +0100

    added EnquiryNumber migration

---
+++ Migrations/20220626114318_EnquiryNumber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Database.MySql.Migrations
+{
+    public partial class EnquiryNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnquiryNumber",
+                table: "Enquiries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnquiryNumber",
+                table: "Enquiries");
+        }
+    }
+}
